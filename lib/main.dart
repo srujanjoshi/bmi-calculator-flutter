538b3cee85e9ce8e40ea45ed1ff49e14d864cd44
srujanjoshi <srujanjoshi7@gmail.com>
2021-11-10 23:29:39 -0500

    Added a static Slider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+
 import 'input_page.dart';
+import 'results_page.dart';
 
 void main() => runApp(BMICalculator());
 
@@ -7,7 +9,11 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: InputPage(),
+      initialRoute: "/",
+      routes: {
+        "/": (context) => InputPage(),
+        "/results": (context) => ResultsPage(),
+      },
       theme: ThemeData.dark().copyWith(
         primaryColor: Color(0xFF0A0D22),
         scaffoldBackgroundColor: Color(0xFF0A0D22),
